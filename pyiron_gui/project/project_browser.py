60405a78b7669bb644ffe596fc3c48b638abc2a4
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2021-05-25 15:55:12 +0200

    use SelectMulti instead of checkboxes

--- pyiron_gui/project/project_browser.py
+++ pyiron_gui/project/project_browser.py
@@ -344,28 +344,31 @@ class DisplayOutputGUI:
         if numpy_array.ndim >= 3:
             self._array_plot_widgets(numpy_array=numpy_array)
             box.children = tuple([
-                widgets.HBox(self._plot_options['dim']),
+                widgets.HBox([self._plot_options['dim']]),
                 widgets.HBox(self._plot_options['idx'])
             ])
         return box
 
     def _array_plot_widgets(self, numpy_array):
-        check_box_layout = widgets.Layout(width='auto', margin='0px 0px 0px -65px')
         shape = numpy_array.shape
         fixed_idx_list = []
-        dim_list = []
-        dim_list.append(widgets.Checkbox(description='Dim 0', layout=check_box_layout, value=True,
-                                         description_tooltip='Plot dimension 0 of the array'))
-        dim_list.append(widgets.Checkbox(description='Dim 1', layout=check_box_layout, value=True,
-                                         description_tooltip='Plot dimension 0 of the array'))
+        dim_widget = widgets.SelectMultiple(description='Plot-Dim', value=[0, 1],
+                                            options=range(numpy_array.ndim),
+                                            rows=3,
+                                            description_tooltip='Plot dimensions of the array '
+                                                                '(exactly 2 choices required)')
+
         for dim in range(numpy_array.ndim-2):
-            dim_list.append(widgets.Checkbox(description='Dim ' + str(2+dim),
-                                             description_tooltip=f'Plot dimension {2+dim} of the array',
-                                             layout=check_box_layout, value=False))
             fixed_idx_list.append(widgets.IntText(description=f'Fixed index {dim}', value=0,
+                                                  layout=widgets.Layout(width='60%'),
                                                   description_tooltip=f'Fixed index of the {dim}th not chosen '
                                                                       f'dimension; the shape of the array is {shape}.'))
-        self._plot_options = {'dim': dim_list, 'idx': fixed_idx_list}
+        if numpy_array.ndim == 3:
+            fixed_idx_list[0].description = 'Fixed index'
+            fixed_idx_list[0].description_tooltip = f'Fixed index of the not chosen dimension; the shape of the ' \
+                                                    f'array is {shape}'
+
+        self._plot_options = {'dim': dim_widget, 'idx': fixed_idx_list}
 
     def plot_array(self, val):
         if self.fig is None:
@@ -380,28 +383,21 @@ class DisplayOutputGUI:
                 self.ax.plot(val[0])
             else:
                 self.ax.plot(val)
-        # elif val.ndim == 3:
-        #    self.ax.plot(val[:, :, 0])
         else:
-            slc = []
-            i = 0
-            for dim_widget in self._plot_options['dim']:
-                if dim_widget.value:
-                    slc.append(slice(None))
-                else:
-                    slc.append(self._plot_options['idx'][i].value)
-                    i += 1
-            if i != val.ndim - 2:
-                print(f"Error: You need to select two dimensions.")
+            if len(self._plot_options['dim'].value) != 2:
+                print(f"Error: You need to select exactly two dimensions.")
                 return
+            slc = [None for i in range(val.ndim)]
+            i = 0
+            for index in range(val.ndim):
+                if index in self._plot_options['dim'].value:
+                    slc[index] = slice(None)
+                else:
+                    slc[index] = self._plot_options['idx'][i].value
+                    i += 1
             else:
                 self.ax.plot(val[tuple(slc)])
 
-        #    print(f"This is a numpy array of dim = {val.ndim}, " +
-        #          "however, plotting is only implemented for up to 3-dimensions.\n")
-        #    return val
-
-        # self.ax.set_title(self._node_name)
         self.ax.relim()
         self.ax.autoscale_view()
         return self.ax.figure
