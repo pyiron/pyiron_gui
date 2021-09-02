5edad8153dcc778d3632c26c1fc581844486290d
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2021-09-02 13:59:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@ setup(
         'pyiron_atomistics==0.2.23',
         'ipywidgets==7.6.3',
         'matplotlib==3.4.3',
-        'mendeleev==0.7.0',
         'numpy==1.21.2',
         'pandas==1.3.2',
     ]
