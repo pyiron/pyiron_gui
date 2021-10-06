a53ad575615f83a62d5b75d94d5cc5bcdede4e32
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2021-10-06 16:21:49 +0200

    Enhance `DisplayOutputGUI` to treat `PyironWrapper` -able objects

--- pyiron_gui/__init__.py
+++ pyiron_gui/__init__.py
@@ -2,7 +2,7 @@ __version__ = "0.1"
 __all__ = []
 
 from pyiron_gui.project.project import activate_gui
-from pyiron_gui.project.project_browser import ProjectBrowser
+from pyiron_gui.project.project_browser import ProjectBrowser, DataContainerGUI
 
 from ._version import get_versions
 __version__ = get_versions()['version']
--- pyiron_gui/project/project_browser.py
+++ pyiron_gui/project/project_browser.py
@@ -66,6 +66,8 @@ class DisplayOutputGUI:
     def display(self, obj, default_output=None):
         if isinstance(obj, BaseWrapper):
             self.display(obj.gui, default_output=default_output)
+        elif isinstance(obj, tuple(PyironWrapper.registry.keys())[1:]):
+            self.display(PyironWrapper(obj, project=None))
         elif isinstance(obj, np.ndarray):
             self.display(NumpyWidget(obj), default_output=default_output)
         else:
@@ -538,6 +540,7 @@ class HasGroupBrowserWithOutput(HasGroupsBrowser):
                 pass
             else:
                 new_project = new_project2
+                self._output.display(new_project)
         self.project = new_project
 
     def _update_project(self, group_name):
@@ -742,24 +745,17 @@ class ProjectBrowser(HasGroupBrowserWithOutput):
             return None
 
     def _update_project_worker(self, rel_path):
+        old_project = self.project
         try:
-            new_project = self.project[rel_path]
-            # Check if the new_project implements list_nodes()
-            if 'TYPE' in new_project.list_nodes():
-                try:
-                    new_project2 = PyironWrapper(new_project.to_object(), self.project, rel_path)
-                except ValueError:  # to_object() (may?) fail with an ValueError for GenericParameters
-                    pass
-                else:
-                    new_project = new_project2
+            super(ProjectBrowser, self)._update_project_worker(rel_path)
         except (ValueError, AttributeError):
             self.path_string_box = self.path_string_box.__class__(description="(rel) Path", value='')
             with self._output:
                 print("No valid path")
             return
         else:
-            if new_project is not None:
-                self.project = new_project
+            if self.project is None:
+                self.project = old_project
 
     def _update_project(self, path):
         self._clear_output()
