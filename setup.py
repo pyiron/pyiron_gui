79dafc405e2cc6782749a28bb69d6f29d81c00ff
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-01-16 00:53:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     keywords='pyiron',
     packages=find_packages(exclude=["*tests*", "*docs*", "*binder*", "*conda*", "*notebooks*", "*.ci_support*"]),
     install_requires=[
-        'pyiron_base==0.6.20',
+        'pyiron_base==0.6.22',
         'pyiron_atomistics==0.4.6',
         'ipywidgets==8.1.1',
         'matplotlib==3.8.2',
