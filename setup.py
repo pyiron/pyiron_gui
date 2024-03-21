255725fafc2486e2060e4f1302a1c4c229ac602a
Niklas Siemer <163834931+niklasiemer@users.noreply.github.com>
2024-03-21 19:06:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
         'nbconvert==7.16.2',
         'nbformat==5.9.2',
         'numpy==1.26.4',
-        'pandas==2.2.0',
+        'pandas==2.2.1',
     ],
     cmdclass=versioneer.get_cmdclass(),
 )
